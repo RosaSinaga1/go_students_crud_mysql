0b3ab6881e04764bfcc2d1e3ef1a14851c7e2ea7
Rosa Sinaga <103059548+RosaSinaga1@users.noreply.github.com>
2022-04-05 23:48:12 +0700

    Add files via upload

---
+++ go_students_crud_mysql/cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Onai23/go_students_crud_mysql/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// inisialisasi routes
+	r := mux.NewRouter()
+	routes.RegisterStudentsRoutes(r)
+	http.Handle("/", r)
+	fmt.Print("Starting Server localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
