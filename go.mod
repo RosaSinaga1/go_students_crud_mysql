1e170b87111b80f914aee943ed4adcdf1b62e18d
Rosa Sinaga <103059548+RosaSinaga1@users.noreply.github.com>
2022-04-06 00:00:04 +0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Onai23/go_students_crud_mysql
+
+go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+)
---
+++ pkg/models/student.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"github.com/Onai23/go_students_crud_mysql/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+//deklarasi cetakan Student
+type Student struct {
+	gorm.Model
+	NIM      string `gorm:""json:"nim"`
+	Name     string `json:"name"`
+	IPK      string `json:"ipk"`
+	Jurusan  string `json:"jurusan"`
+	Angkatan string `json:"angkatan"`
+}
+
+//set fungsi init()
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Student{})
+}
+
+//fungsi CreateStudent()
+func (b *Student) CreateStudent() *Student {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+//set fungsi GetAllStudents()
+func GetAllStudents() []Student {
+	//set objek cetakan Student
+	var Students []Student
+	db.Find(&Students)
+	return Students
+}
+
+//set fungsi GetStudentById()
+func GetStudentById(nim int64) (*Student, *gorm.DB) {
+	//set objek cetakan Student
+	var getStudent Student
+	db := db.Where("NIM=?", nim).Find(&getStudent)
+	return &getStudent, db
+}
+
+//set fungsi DeleteStudent()
+func DeleteStudent(nim int64) Student {
+	//set objek cetakan Student
+	var student Student
+	db.Where("NIM=?", nim).Delete(student)
+	return student
+}
